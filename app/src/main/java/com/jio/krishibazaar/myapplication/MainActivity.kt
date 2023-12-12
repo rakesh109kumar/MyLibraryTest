f882ebf3eec21eb04cf917c9d669ecd97caee2b2
Rakesh109.Kumar <rakesh109.kumar@ril.com>
2023-12-12 16:20:00 +0530

    Initial commit - TestClass added

---
+++ app/src/main/java/com/jio/krishibazaar/myapplication/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.jio.krishibazaar.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.ui.*
+import com.compose.test.mylibrary_test.TestClass
+import com.jio.krishibazaar.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        binding.navView.setNavigationItemSelectedListener(this)
+        //setClick()
+        TestClass()
+    }
+
+    fun setClick() {
+        binding.navView.setNavigationItemSelectedListener{
+            when (it.itemId) {
+
+                R.id.nav_gallery -> {
+                    //write your implementation here
+                    if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                        binding.drawerLayout.closeDrawer(GravityCompat.START)
+                    }
+
+                    true
+                }
+                R.id.nav_slideshow -> {
+                    if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                        binding.drawerLayout.closeDrawer(GravityCompat.START)
+                    }
+                    binding.navView.setCheckedItem(R.id.nav_slideshow)
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+
+
+
+            /*when(it.itemId){
+                R.id.nav_home -> {
+
+                }
+                R.id.nav_gallery -> {
+
+                }
+                R.id.nav_slideshow -> {
+
+                }
+
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            true*/
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val navigationSelected = NavigationUI.onNavDestinationSelected(item, navController)
+        when(item.itemId){
+            R.id.nav_home -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_gallery ->{
+
+            }
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return navigationSelected
+    }
+}
---
+++ mylibrary-test/src/main/java/com/compose/test/mylibrary_test/TestClass.kt
@@ -0,0 +1,10 @@
+package com.compose.test.mylibrary_test
+
+import android.util.Log
+
+class TestClass {
+
+    fun testClassFun(){
+        Log.e("TestClass","TestClass>>>>testFun")
+    }
+}
